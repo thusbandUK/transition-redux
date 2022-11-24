51209be57a290546b5d0e8e5896265afcc5f94d4
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2022-11-24 04:55:44 +0000

    styling, buttons placed in a row for examboard selection, midway through color scheme update

--- src/features/examBoards/ExamBoards.js
+++ src/features/examBoards/ExamBoards.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectExamBoard } from './examBoardsSlice';
 import'../../app/App.css';
+import './examBoards.css';
 import { data } from '../../data';
 
 export function ExamBoards() {    
@@ -10,15 +11,24 @@ export function ExamBoards() {
     return (
       <div>
           <h1>Select Exam Board</h1>
+          <ul className="list-group list-group-horizontal mt-5 fs-5">
           {data.examBoards.map((examBoardEntry) => (
+            <li
+              className="list-group-item  w-50"              
+            >
               <button 
-              className="list-group mt-4 fs-5 list-group-item bg-light"
+              className="d-block text-decoration-none"
               onClick={() => dispatch(selectExamBoard({examBoard: examBoardEntry}))}
               >
                   {examBoardEntry}  
               </button>
+            </li>
           ))}
+          </ul>
       </div>    
     );
   }
 
+  //className="list-group mt-4 fs-5 list-group-item bg-light"
+
+  //bg-light
--- src/features/menu/Menu.js
+++ src/features/menu/Menu.js
@@ -11,6 +11,8 @@ const Menu = () => {
     const reagentOptions = useSelector(state => state.menu.reagentOptions);
     const dispatch = useDispatch()
     
+
+
     useEffect(() => {   
           const reagentOptions = data.reagentOptions.filter((reagent) => {
             if (reagent.true.includes(examBoard)){
@@ -22,30 +24,23 @@ const Menu = () => {
           })
       dispatch(selectReagentOptions(reagentOptions))
   }, [dispatch, examBoard]);
+  
+  
 
+//BELOW REPLACES ABOVE (MOTHBALLED)
 /*
-
-SWITCH THE BELOW FOR THE ABOVE AT SOME POINT
-
 useEffect(() => {  
-          let reagentOptions; 
+          let reagentOptions = []; 
           data.reagentOptions.map((reagent) => {
-            if (reagent.true.includes(examBoard)){
-              delete reagent.true;
-              reagentOptions.push(reagent);             
+            if (reagent.true.includes(examBoard)){              
+              reagentOptions.push(reagent);     
+              delete reagent.true;        
             }          
           })
       dispatch(selectReagentOptions(reagentOptions))
-  }, [examBoard]);
+  }, [dispatch, examBoard]);
 
-
-
-
-*/
-
-
-
-
+  */
 
       const {reactant} = useParams();
 
@@ -86,19 +81,11 @@ useEffect(() => {
         }
       }
 
-      /*
-      what needs to happen here is you need to take work off of the computational aspects of this module and refer back to data.js
-      but also you need to dispatch the whole object as selected reagent to make the logic easier on each actual reagent page
-      the "with" is not really helping you in the path because it doesn't make much sense without having "reactions" before it, if you
-      get rid of that then the menu writer is already easier. But menu writer, rather than making a new object simply needs to 
-      return from data.js the existing object that goes with the reagent. That's what menu writer needs to do: return that object
-      and then the other logic bends to that
-      */
-
+      
 return (
-<div>
+        <div>
   
-<ul className="list-group mt-4 fs-5" style={reactant ? {display: 'none'} : {display: 'flex'}}>
+          <ul className="list-group mt-4 fs-5" style={reactant ? {display: 'none'} : {display: 'flex'}}>
             {reagentOptions.map((reagent) =>               
               (<li
                 className="list-group-item bg-light"
@@ -114,67 +101,17 @@ return (
                </li>)
             )}            
           </ul>
-{/**/}
+
           <ul className="list-group list-group-horizontal mt-5 fs-5" style={reactant ? {display: 'flex'} : {display: 'none'}}>
-                <li className="list-group-item bg-light w-50"><NavLink className="d-block text-decoration-none" to={menuWriter(reactant, 'back').path}  onClick={() => dispatch(selectReagent(menuWriter(reactant, 'back')))}><i className="mdi mdi-chevron-left"></i>Previous section: <strong>{menuWriter(reactant, 'back').linkText ? menuWriter(reactant, 'back').linkText : menuWriter(reactant, 'back').name}</strong></NavLink></li>
-                <li className="list-group-item bg-light w-50"><NavLink className="d-block text-decoration-none text-end" to={menuWriter(reactant, 'forward').path} onClick={() => dispatch(selectReagent(menuWriter(reactant, 'forward')))}>Next section: <strong>{menuWriter(reactant, 'forward').linkText ? menuWriter(reactant, 'forward').linkText : menuWriter(reactant, 'forward').name}</strong><i className="mdi mdi-chevron-right"></i></NavLink></li>
-            </ul>
+            <li className="list-group-item bg-light w-50"><NavLink className="d-block text-decoration-none" to={menuWriter(reactant, 'back').path}  onClick={() => dispatch(selectReagent(menuWriter(reactant, 'back')))}><i className="mdi mdi-chevron-left"></i>Previous section: <strong>{menuWriter(reactant, 'back').linkText ? menuWriter(reactant, 'back').linkText : menuWriter(reactant, 'back').name}</strong></NavLink></li>
+            <li className="list-group-item bg-light w-50"><NavLink className="d-block text-decoration-none text-end" to={menuWriter(reactant, 'forward').path} onClick={() => dispatch(selectReagent(menuWriter(reactant, 'forward')))}>Next section: <strong>{menuWriter(reactant, 'forward').linkText ? menuWriter(reactant, 'forward').linkText : menuWriter(reactant, 'forward').name}</strong><i className="mdi mdi-chevron-right"></i></NavLink></li>
+          </ul>
               
-          </div>);
+        </div>
+  );
 
-              }
+}
 
-              export default Menu;
+export default Menu;
 
-              /*
-
-//onClick={() => dispatch(selectReagent({reagent: menuWriter(reactant, 'back').name}))}
-//{menuWriter(reactant, 'back').linkText}
-
-
-//onClick={() => dispatch(selectReagent({reagent: menuWriter(reactant, 'forward').name}))}
-//{menuWriter(reactant, 'forward').linkText}
-
-
-
-
-              const menuWriter = (currentUrl, direction) => {
-        let reagentObject = {
-          name: '',
-          path: '',
-          linkText: ''
-        };
-        if (!reactant) {
-          reagentObject = {name: '', path: '/'};
-         return reagentObject;
-       }
-        //const separatedWords = currentUrl.split('_');
-        //const withRemoved = separatedWords.slice(1, separatedWords.length);
-        // withRemoved.join(' ')
-        const stringWithSpaces = dashBeGone(currentUrl);
-        const currentSection = reagentOptions.findIndex((x) => x.name === stringWithSpaces);        
-        
-        
-
-        if (direction === 'forward'){
-            if (currentSection === (reagentOptions.length - 1)){
-              reagentObject = {name: '', linkText: 'back to main menu', path: '/'};
-                
-            } else {
-              const backReagent = reagentOptions[(currentSection + 1)].name;
-               reagentObject = {name: backReagent, linkText: backReagent, path: `/${spaceBeGone(reagentOptions[(currentSection + 1)].name)}`};
-                     
-            }
-        }
-        else if (direction === 'back'){
-            if (currentSection === 0){
-              reagentObject = {name: '', linkText:'back to main menu', path: '/'};
-            } else {
-            const forwardReagent = reagentOptions[(currentSection - 1)].name;  
-            reagentObject = {name: forwardReagent, linkText: forwardReagent, path: `/${spaceBeGone(reagentOptions[currentSection - 1].name)}`};
-        }
-        }
-        return reagentObject;
-    }
               
-              */
